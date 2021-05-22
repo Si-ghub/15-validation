308277286499e6305b70f604c922ada2ee685730
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-05-22 12:00:58 +0300

    Message validation

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const Validation = require('./js/Validation');
+
+const v = new Validation();
+
+console.log(v.isValidMessage('Some message'), true);
+console.log(v.isValidMessage(12345), false);
+console.log(v.isValidMessage('12345'), true);
+console.log(v.isValidMessage(true), false);
+console.log(v.isValidMessage([12, 15]), false);
+console.log(v.isValidMessage([12, 15, 12]), false);
+
+
+//console.log(v.isValidFirstName('Vardenis'), true);
+//console.log(v.isValidLastName('Pavardenis'), true);
+//console.log(v.isValidEmail('demo@excample.com'), true);
+//console.log(v.isValidMessage('Some message'), true);
+//console.log(v.isValidPhoneNumber(+37065954415), true);
+//console.log(v.isValidMonthName('Sausis'), true);
+//console.log(v.isValidMonthName('January'), true);
+//console.log(v.isValidWeekdayName('Pirmadienis'), true);
+//console.log(v.isValidWeekdayName('Monday'), true);
+//console.log(v.isValidPersonID('39912311234'), true);
+//
--- js/Validation.js
+++ js/Validation.js
@@ -1,3 +1,40 @@
 class Validation {
-    
+    isValidFirstName() {
+        return true;
+    }
+
+    isValidLastName() {
+        return true;
+    }
+
+    isValidEmail() {
+        return true;
+    }
+
+    isValidMessage (message) {
+        if(typeof message !== 'string' || message === '') {
+            return false;
+
+        }
+            return true;
+    }
+
+    isValidPhoneNumber () {
+        return true;
+    }
+
+    isValidMonthName () {
+        return true;
+    }
+
+    isValidWeekdayName() {
+        return true;
+    }
+
+    isValidPersonID() {
+        return true;
+    }
+
 }
+
+module.exports = Validation;
