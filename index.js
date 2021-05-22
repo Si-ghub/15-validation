fe0f2317fa241db1e7f45edd58336063eb96cd7c
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-05-22 14:07:22 +0300

    Valid

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const Validation = require('./js/Validation');
 
 const v = new Validation();
 
+//TESTAI 
+
+//IS VALID MESSAGE
 console.log(v.isValidMessage('Some message'), true);
 console.log(v.isValidMessage(12345), false);
 console.log(v.isValidMessage('12345'), true);
@@ -9,15 +12,52 @@ console.log(v.isValidMessage(true), false);
 console.log(v.isValidMessage([12, 15]), false);
 console.log(v.isValidMessage([12, 15, 12]), false);
 
+//IS VALID FIRST NAME
+console.log(v.isValidFirstName('Vardenis'), true);
+console.log(v.isValidFirstName('Pavardenis'), false);
+console.log(v.isValidFirstName('12345'), false);
+console.log(v.isValidFirstName(''), false);
+console.log(v.isValidFirstName([]), false);
+console.log(v.isValidFirstName('P'), false);
+console.log(v.isValidFirstName('Sigita'), true);
+console.log(v.isValidFirstName('sigita'), false);
+console.log(v.isValidFirstName('siGita'), false);
+
+
+
+//IS VALID LAST NAME
+console.log(v.isValidLastName('Pavardenis'), true);
+console.log(v.isValidLastName('Vardenis'), false);
+console.log(v.isValidLastName('12345'), false);
+console.log(v.isValidLastName(''), false);
+console.log(v.isValidLastName([]), false);
+console.log(v.isValidLastName('Sigita'), true);
+console.log(v.isValidLastName('sigita'), false);
+console.log(v.isValidLastName('siGita'), false);
+
+
+
+
+
+
+
+
+
+
+//IS VALID PHONE NUMBER
+console.log(v.isValidPhoneNumber(+37065954415), true);
+
+
+
 
 //console.log(v.isValidFirstName('Vardenis'), true);
 //console.log(v.isValidLastName('Pavardenis'), true);
 //console.log(v.isValidEmail('demo@excample.com'), true);
 //console.log(v.isValidMessage('Some message'), true);
 //console.log(v.isValidPhoneNumber(+37065954415), true);
-//console.log(v.isValidMonthName('Sausis'), true);
-//console.log(v.isValidMonthName('January'), true);
+//console.log(v.isValidMonthName('Sausis', 'kalba'), true);
+//console.log(v.isValidMonthName('January, 'kalba'), true);
 //console.log(v.isValidWeekdayName('Pirmadienis'), true);
 //console.log(v.isValidWeekdayName('Monday'), true);
 //console.log(v.isValidPersonID('39912311234'), true);
-//
+
--- js/Validation.js
+++ js/Validation.js
@@ -1,9 +1,35 @@
 class Validation {
-    isValidFirstName() {
+    isValidFirstName(firstName) {
+        // blogai jeigu ne stringas
+        // blogai jeigu simboliu yra maziau nei du
+        // blogai jei pirmoji vardo raide===mazoji
+        // blogai jeigu 
+        if (!this.isValidMessage(firstName)) {
+            return false;
+        }
+        if (firstName.lenght < 2) {
+            return false;
+        }
+        if (firstName[0].toLowerCase() === firstName[0]) {
+            return false;
+        }
+        if (firstName.slice(1).toLowerCase() !== firstName.slice(1)) {
+            return false;
+        }
         return true;
+
     }
 
-    isValidLastName() {
+    isValidLastName(lastName) {
+        // blogai jeigu ne stringas
+        // blogai jeigu simboliu yra maziau nei du
+        // blogai jeigu pirmoji vardo raide===mazoji
+        if (!this.isValidMessage(lastName)) {
+            return false;
+        }
+        if (lastName.lenght < 2) {
+            return false;
+        }
         return true;
     }
 
@@ -11,19 +37,20 @@ class Validation {
         return true;
     }
 
-    isValidMessage (message) {
-        if(typeof message !== 'string' || message === '') {
+    isValidMessage(message) {
+        // blogai jeigu nera stringas
+        // blogai jeigu tekstas 'tuscia'
+        if (typeof message !== 'string' || message === '') {
             return false;
-
         }
-            return true;
-    }
-
-    isValidPhoneNumber () {
         return true;
     }
 
-    isValidMonthName () {
+    isValidPhoneNumber() {
+        return true;
+    }
+
+    isValidMonthName() {
         return true;
     }
 
